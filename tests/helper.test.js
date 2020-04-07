e728c99e78f9220a44e4db11fb40ba0318ac7435
shivam <shivam.kaushik@outlook.com>
2020-04-07 15:47:35 +0530

    Finish step3 of helper test

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,4 +1,4 @@
-const { dummy, totalLikes } = require('./../utils/list_helper')
+const { dummy, totalLikes, favoriteBlog } = require('./../utils/list_helper')
 
 test('dummy returns one', () => {
   const blogs = []
@@ -62,5 +62,53 @@ describe('total likes', () => {
 
   test('of a bigger list is calculated right', () => {
     expect(totalLikes(listWithMultipleBlog)).toBe(30)
+  })
+})
+
+describe('favourite blog', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 15,
+      __v: 0,
+    },
+  ]
+
+  test('should return blog with max likes', () => {
+    expect(favoriteBlog(blogs)).toEqual({
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url:
+        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 15,
+      __v: 0,
+    })
+  })
+
+  test('should return undefined when empty blog is provided', () => {
+    expect(favoriteBlog([])).toBe(undefined)
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,14 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((prev, curr) => prev + curr.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes)
+  const maxLikes = Math.max(...likes)
+  return blogs.find((blog) => blog.likes === maxLikes)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
