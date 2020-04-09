2d2eb1598118bbb1593e928a72e9159be22934f4
shivam <shivam.kaushik@outlook.com>
2020-04-09 12:26:58 +0530

    Finish step2 of bloglists test

---
+++ tests/persons_api.test.js
@@ -0,0 +1,26 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const Blog = require('./../models/blog')
+const app = require('./../app')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany()
+  for (const blog of helper.initialBlogs) {
+    await Blog.create(blog)
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
---
+++ tests/test_helper.js
@@ -0,0 +1,38 @@
+const Blog = require('./../models/blog')
+
+const initialBlogs = [
+  {
+    name: 'blog 1',
+    author: 'author 1',
+    url: 'www.blog1.com',
+    likes: 25,
+  },
+  {
+    name: 'blog 2',
+    author: 'author 2',
+    url: 'www.blog2.com',
+    likes: 25,
+  },
+  {
+    name: 'blog 3',
+    author: 'author 3',
+    url: 'www.blog3.com',
+    likes: 25,
+  },
+  {
+    name: 'blog 4',
+    author: 'author 4',
+    url: 'www.blog4.com',
+    likes: 25,
+  },
+]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find()
+  return blogs.map((blog) => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs,
+  blogsInDb,
+}
