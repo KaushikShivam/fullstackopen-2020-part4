80c8d0ff85718bf73e597eaf23d61541f7c9b351
shivam <shivam.kaushik@outlook.com>
2020-04-09 13:39:53 +0530

    Finish 4.13 exercise

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,4 +13,9 @@ blogRouter.post('/', async (request, response, next) => {
   response.status(201).json(savedBlog.toJSON())
 })
 
+blogRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
 module.exports = blogRouter
--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -84,3 +84,14 @@ test('blog without content is not added', async () => {
 
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
+
+test('blog is deleted successfully', async () => {
+  const blogs = await helper.blogsInDb()
+  const blogToDelete = blogs[0]
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+})
