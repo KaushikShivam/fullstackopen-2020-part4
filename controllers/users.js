5bce796549606b35abb4aaffdf87962971aa4df2
shivam <shivam.kaushik@outlook.com>
2020-04-09 19:04:24 +0530

    Finish step 5 bloglist

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const passwordHash = await bcrypt.hash(body.password, 12)
+
+  const user = await User.create({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
+
+  response.status(201).json(user)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find()
+  response.status(200).json(users.map((user) => user.toJSON()))
+})
+
+module.exports = usersRouter
