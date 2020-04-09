714a807c7969b596270eee4d859907c4efc4735c
shivam <shivam.kaushik@outlook.com>
2020-04-09 12:30:54 +0530

    Finish step3 of bloglists test

--- tests/persons_api.test.js
+++ tests/persons_api.test.js
@@ -24,3 +24,10 @@ test('correct amount of blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
+
+test('unique identifier id is defined', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach((blog) => {
+    expect(blog.id).toBeDefined()
+  })
+})
